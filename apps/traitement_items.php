3e5c49385024a68934ff1907744dba96b2191a58
jibs13 <jb.sabatier@mail.com>
2017-02-27 14:24:43 +0100

    item_manager

--- apps/traitement_items.php
+++ apps/traitement_items.php
@@ -1,3 +1,28 @@
 <?php
+if(isset($_POST['name'], $_POST['id_category'], $_POST['stock'], $_POST['price'], $_POST['description'], $_SESSION['id']))
+{
+	$manager = new ItemManager($db);
+	$categoryManager = new categoryManager($db);
+
+	try
+	{
+		$item = $manager->create($_POST['description'], $_POST['name'], $_POST['id_category'], $_POST['stock'],  $_POST['price']);
+		// $description, $name, $id_category, $stock, $price
+
+		if($item)
+		{
+			header('Location: index.php?page=items&id='.$item->getId());
+			exit;
+		}
+		else
+		{
+			$errors[] = "Erreur interne(items)";
+		}
+	}
+	catch(Execptions $e)
+	{
+		$errors = $e->getErrors();
+	}
+}
 
 ?>
