11fa6d57e073297506c62710b08bfe87d9fd7a2f
nellygrr <neeeeelly@gmail.com>
2017-02-27 14:21:15 +0100

    apps/traitement_command.php

---
+++ apps/traitement_command.php
@@ -0,0 +1,32 @@
+<?php
+// var_dump($_POST);
+if (isset($_POST['price'], $_POST['status'], $_SESSION['id']))
+{
+	// Etape 2
+	$userManager = new UserManager($db);
+	$customer = $userManager->findById($_SESSION['id']);
+	$manager = new CommentManager($db);
+	try
+	{
+		// Etape 3
+		//           public function create($price, $id_author, $status) -> CommentManager.class.php ligne 47
+		//           public function create($price, User $author, $status) -> CommentManager.class.php ligne 47
+		// $command = $manager->create($_POST['price'], $_SESSION['id'], $_POST['status']);
+		$command = $manager->create($_POST['price'], $author, $_POST['status']);
+		if ($command)
+		{
+			// Etape 4
+			header('Location: index.php?page=article&id='.$command->getIdCustomer());
+			exit;
+		}
+		else
+		{
+			$errors[] = "Erreur interne";
+		}
+	}
+	catch (Exceptions $e)
+	{
+		$errors = $e->getErrors();
+	}
+}
+?>
