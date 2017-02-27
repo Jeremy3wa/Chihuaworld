71a5cd810eb6ff51abd4f8c1dbdb049aa6324c85
setara67 <setara@live.fr>
2017-02-27 15:27:43 +0100

    apps nat

---
+++ apps/item_elem.php
@@ -0,0 +1,13 @@
+<?php
+/*while ($article = mysqli_fetch_assoc($res))
+{
+	require('views/articles_elem.phtml');
+}*/
+$count = 0;
+while ($count < count($list))// list.length
+{
+	$article = $list[$count];
+	require('views/item_elem.phtml');
+	$count++;
+}
+?>
