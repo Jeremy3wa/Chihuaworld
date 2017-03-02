a1d47a4a462d40f343b2a62c5357390066914ae4
Jeremy3wa <Jeremy.6796@hotmail.fr>
2017-03-02 15:29:09 +0100

    jerem

--- apps/traitement_items.php
+++ apps/traitement_items.php
@@ -1,30 +1,37 @@
 <?php
-
-if(isset($_POST['name'], $_POST['id_category'], $_POST['stock'], $_POST['size'], $_POST['price'], $_POST['description'],$_POST['picture'], $_SESSION['id']))
+var_dump($_POST);
+if (isset($_POST['action']))
 {
-	$manager = new ItemManager($db);
-	$categoryManager = new CategoryManager($db);
-	$category = $categoryManager->findById($_POST['id_category']);
-
-	try
+	$action = $_POST['action'];
+	if ($action == "create_item")
 	{
-		$item = $manager->create($_POST['description'], $_POST['name'], $category, $_POST['stock'], $_POST['size'], $_POST['price'], $_POST['picture']);
-
-		// $description, $name, $id_category, $stock, $price
-
-		if($item)
+		if(isset($_POST['name'], $_POST['id_category'], $_POST['stock'], $_POST['size'], $_POST['price'], $_POST['description'],$_POST['picture'], $_SESSION['id']))
 		{
-			header('Location: index.php?page=items&id='.$item->getId());
-			exit;
+			$manager = new ItemManager($db);
+			$categoryManager = new CategoryManager($db);
+			$category = $categoryManager->findById($_POST['id_category']);
+
+			try
+			{
+				$item = $manager->create($_POST['description'], $_POST['name'], $category, $_POST['stock'], $_POST['size'], $_POST['price'], $_POST['picture']);
+
+				// $description, $name, $id_category, $stock, $price
+
+				if($item)
+				{
+					header('Location: index.php?page=items&id='.$item->getId());
+					exit;
+				}
+				else
+				{
+					$errors[] = "Erreur interne(items)";
+				}
+			}
+			catch(Exceptions $e)
+			{
+				$errors = $e->getErrors();
+			}
 		}
-		else
-		{
-			$errors[] = "Erreur interne(items)";
-		}
-	}
-	catch(Exceptions $e)
-	{
-		$errors = $e->getErrors();
 	}
 }
 
