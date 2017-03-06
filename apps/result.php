1074f5c5254bdb525c9c7f906639989101eabcd8
jibs13 <jb.sabatier@mail.com>
2017-03-06 14:31:27 +0100

    result

--- apps/result.php
+++ apps/result.php
@@ -1,32 +1,32 @@
 <div class="container">
-			<div class="main-content">
-				<div class="online-strip">
-					
-				<div class="products-grid">
+	<div class="main-content">
+		<div class="online-strip">
+			
+			<div class="products-grid">
 				<header>
 					<h3 class="head text-center">Nos produits</h3>
 				</header>
 				
 
-<?php
-if(isset($_GET['recherche']))
-{
-  $recherche = mysqli_real_escape_string($db, $_GET["mot"]);
-  $res= mysqli_query($db, "SELECT * FROM items WHERE name COLLATE utf8_unicode_ci LIKE '%$recherche%'");
-  while($item = mysqli_fetch_object($res, 'Item', [$db])) 
-  {  
-    require ("views/result.phtml");
-  } 
-}
-?>
+				<?php
+				if(isset($_GET['recherche']))
+				{
+					$recherche = mysqli_real_escape_string($db, $_GET["mot"]);
+					$res= mysqli_query($db, "SELECT * FROM items WHERE name COLLATE utf8_unicode_ci LIKE '%$recherche%'");
+					while($item = mysqli_fetch_object($res, 'Item', [$db])) 
+					{  
+						require ("views/result.phtml");
+					} 
+				}
+				?>
 
 
 
-				</div>
-
-				<br>
-				<div class="clearfix"></div>
 			</div>
 
+			<br>
+			<div class="clearfix"></div>
 		</div>
-<div>
+
+	</div>
+	<div>
